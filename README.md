0e78ea9d6cb59b2c99d842da3d10f7e3d8b967b2
jsmac2022 <jitendra.sharma@mactosys.com>
2022-12-28 16:27:02 +0530

    first commit 28dec22

---
+++ README.md
@@ -0,0 +1,23 @@
+# CCAvenue_Payment_gateway_Integration_Android
+## CCavenue payment gatway integration in android, with server-side implementaion 
+
+### dependencies
+#### implementation 'com.jakewharton:butterknife:6.1.0'
+#### implementation 'com.android.volley:volley:1.1.0'
+
+#### for server-side implementation we need to download 4 files from ccavenue dashboard which is (Crypto.php,cacert.pem,GetRSA.php,ccavMobileResponseHandler.php).
+#### you can download .jsp files based on your requiremnet, so here I am using php files
+
+### Output
+
+<img src="https://user-images.githubusercontent.com/28274092/162584771-bf13e0a8-de7c-4ccb-82db-8097b14ff4de.jpg" width="200" height="400" />
+
+<img src="https://user-images.githubusercontent.com/28274092/162584405-fb923842-ff03-47d4-b811-0f145e982c96.jpg" width="200" height="400" />
+
+
+<img src="https://user-images.githubusercontent.com/28274092/162584372-02a6141e-f54f-41be-8084-c546b1ab5223.jpg" width="200" height="400" />
+
+
+<img src="https://user-images.githubusercontent.com/28274092/162584380-ca4d7cc3-c810-4865-a304-7b5f9885f238.jpg" width="200" height="400" />
+
+
---
+++ app/src/main/java/com/acpitzone/ccavenue_payment/Utility/Constants.java
@@ -0,0 +1,20 @@
+package com.acpitzone.ccavenue_payment.Utility;
+
+public class Constants {
+
+	public static final String PARAMETER_SEP = "&";
+	public static final String PARAMETER_EQUALS = "=";
+	public static final String TRANS_URL = "https://secure.ccavenue.com/transaction/initTrans";
+	public static final String access_code = "AVVS90JF12BR25SVRB"; //4YRUXLSRO20O8NIH   add your access_code
+	public static final String merchantId= "1004089"; //2  add your merchant_id
+	public static final String currency="INR";
+
+//	public static final String redirectUrl="http://122.182.6.216/merchant/ccavResponseHandler.jsp"; //this is test url you can modify with your on url, you can use php or jsp,
+//	public static final String cancelUrl="http://122.182.6.216/merchant/ccavResponseHandler.jsp";//this is test url you can modify with your on url
+//	public static final String rsaKeyUrl="https://secure.ccavenue.com/transaction/jsp/GetRSA.jsp"; //this is test url you can modify with your on url
+
+	public static final String redirectUrl="https://schoolforme.in:5000/api/common/ccavRequestHandler:{{#formdata}}"; //this is test url you can modify with your on url, you can use php or jsp,
+	public static final String cancelUrl="https://schoolforme.in:5000/api/common/ccavRequestHandler:{{#formdata}}";//this is test url you can modify with your on url
+	public static final String rsaKeyUrl="https://secure.ccavenue.com/transaction/jsp/GetRSA.jsp"; //this is test url you can modify with your on url
+
+}
